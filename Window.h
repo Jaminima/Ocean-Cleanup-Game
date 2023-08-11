386b798a0158f58ba132d0dbf0e30427f038461d
Oscar Davies <oscar.william.davies@outlook.com>
2023-08-11 22:32:12 +0100

    Mouse Pan

--- Window.h
+++ Window.h
@@ -4,29 +4,50 @@
 
 void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
+	float moveStep = 0.1f;
+
 	switch (key) {
 	case 'W':
-		sceneObjs.cam.updatePosition(vec3(0, 0, 1));
+		sceneObjs.cam.updatePosition(vec3(0, 0, moveStep));
 		break;
 	case 'S':
-		sceneObjs.cam.updatePosition(vec3(0, 0, -1));
+		sceneObjs.cam.updatePosition(vec3(0, 0, -moveStep));
 		break;
 	case 'A':
-		sceneObjs.cam.updatePosition(vec3(1, 0, 0));
+		sceneObjs.cam.updatePosition(vec3(moveStep, 0, 0));
 		break;
 	case 'D':
-		sceneObjs.cam.updatePosition(vec3(-1, 0, 0));
+		sceneObjs.cam.updatePosition(vec3(-moveStep, 0, 0));
 		break;
 	case 'Z':
-		sceneObjs.cam.updatePosition(vec3(0, -1, 0));
+		sceneObjs.cam.updatePosition(vec3(0, -moveStep, 0));
 		break;
 	case 'X':
-		sceneObjs.cam.updatePosition(vec3(0, 1, 0));
+		sceneObjs.cam.updatePosition(vec3(0, moveStep, 0));
 		break;
-
+	case ']':
+		exit(0);
+		break;
 	}
 
 	sceneObjs.cam.updateMatrix();
+}
+
+double mouseX, mouseY;
+
+void MouseCallback(GLFWwindow* window, double xpos, double ypos)
+{
+	float step = 0.1f;
+
+	double dx = mouseX - xpos;
+	double dy = mouseY - ypos;
+
+	sceneObjs.cam.rotation += vec3(dy * step, dx * step, 0);
+
+	sceneObjs.cam.updateMatrix();
+
+	mouseX = xpos;
+	mouseY = ypos;
 }
 
 void initWindow() {
@@ -40,6 +61,8 @@ void initWindow() {
 	InitRenderer();
 
 	glfwSetKeyCallback(window, KeyCallback);
+	glfwSetCursorPosCallback(window, MouseCallback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	while (!glfwWindowShouldClose(window))
 	{
