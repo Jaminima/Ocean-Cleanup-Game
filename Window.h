cde338b653dba740e5cab71ab01a92e2993d8f9f
Oscar Davies <oscar.william.davies@outlook.com>
2023-08-13 16:32:44 +0100

    Add Message

--- Window.h
+++ Window.h
@@ -16,6 +16,11 @@ void initWindow() {
 	glfwSetMouseButtonCallback(window, MouseButtonCallback);
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
+	system("cls");
+
+	printf("Controls:\nWASD - Move\nX/Z - Up/Down\nMouse - Look\nLeft Click - Pickup Item\n\nR - Restart\n] - Quit\n\n");
+	printf("The Aim Of The Game Is To Collect Rubbish & Fish From The Ocean.\nHigher The Score The Better!\nMake Sure You Keep An Eye On Your Breath!\n\n");
+
 	while (!glfwWindowShouldClose(window))
 	{
 		// uncomment to draw only wireframe
